a28d8f3af596c5df821eaa383cd338b45b43bf56
Martin Hinshelwood (nkdALM) <martin@nakedalm.com>
2015-02-16 15:45:25 +0100

    added files

--- TfsWitAnnotateField.UI/Properties/AssemblyInfo.cs
+++ TfsWitAnnotateField.UI/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tfs Field Annotate")]
-[assembly: AssemblyDescription("Annotates any Field to see who did what")]
+[assembly: AssemblyDescription("Who messed with my fields?")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Martin Hinshelwood")]
+[assembly: AssemblyCompany("Tfs Field Annotate")]
 [assembly: AssemblyProduct("TfsWitAnnotateField.UI")]
-[assembly: AssemblyCopyright("Copyright © Martin Hinshelwood 2012")]
+[assembly: AssemblyCopyright("Copyright © naked ALM Consulting 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
